a2c02383ac33992a9c7cb969a1d0ae57dc78fb9d
luwenjiechn <luwenjiechn@qq.com>
2017-01-20 23:09:42 +0800

    default

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 const request = require('request-promise')
 
-request('http://s.music.163.com/search/get?s=玫瑰色的你&limit=20&type=1&offset=0')
-    .then(data=>console.log(data))
+const baseUrl = 'http://music.163.com/api'
+
+const api = new class {
+    // 搜索
+    async search(s = null, {limit = 10, type = 1, offset = 0} = {}) {
+        const uri = 'http://s.music.163.com/api/search/get/'
+        const options = {
+            uri,
+            qs: { s, limit, type, offset }
+        }
+        const data = await request(options)
+        return data
+    }
+
+    // 歌曲详情
+    async play(id) {
+        const uri = `${baseUrl}/song/detail`
+        const options = {
+            uri,
+            qs: { id, ids: `[${id}]` }
+        }
+        const data = await request(options)
+        return data
+    }
+
+    // 获取歌手专辑列表
+    async getArtistAlbums(artistId = null, {limit = 10, offset = 0}) {
+        const uri = `${baseUrl}/artist/albums/${artistId}`
+        const options = {
+            uri,
+            qs: { limit, offset }
+        }
+        const data = await request(options)
+        return data
+    }
+
+    // 获取专辑音乐列表
+    async getAlbum(albumId = null) {
+        const uri = `${baseUrl}/album/${albumId}`
+        const options = { uri }
+        const data = await request(options)
+        return data
+    }
+
+}()
+
+module.exports = api
--- test.js
+++ test.js
@@ -1,2 +1,6 @@
 const api = require('./index.js')
 
+// api.search('你好吗').then(data=>console.log(data))
+// api.play(28377211).then(data => console.log(data))
+// api.getArtistAlbums('9952').then(data => console.log(data))
+// api.getAlbum(32311).then(data => console.log(data))
