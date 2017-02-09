3c2d12447856657a59a82f1683a82aeb87fe6904
luwenjiechn <luwenjiechn@qq.com>
2017-02-09 17:28:20 +0800

    update

--- test.js
+++ test.js
@@ -11,6 +11,6 @@ const test = (data) => {
 
 api.search('你好吗').then(test)
 api.play(28377211).then(test)
+api.playlist(311785002).then(test)
 api.getArtistAlbums('9952').then(test)
 api.getAlbum(32311).then(test)
-api.playlist(311785002).then(test)
